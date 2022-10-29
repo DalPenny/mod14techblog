6b751c6d8e59309e25009ad5ef4219c0a680ed20
DalPenny <105093756+DalPenny@users.noreply.github.com>
2022-10-28 20:20:52 -0500

    server.js//added helpers and handlebar routes, controllers/homeroutes.js, in controller homepage, update the join in postblog and user to get data, added controller/index.js to route homepage and api, utils/helpers.js added date formatting, vewis/homepage.handlebars and veiws/layouts/main.handlebars added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,60 +4,33 @@ const withAuth = require('../utils/auth');
 
 
 router.get('/', async (req, res) => {
-    // if (!req.session.logged_in) {
-    res.render('homepage');
-    const blogData = await Blogpost.findAll({
-        attributes: ["id", "blogtitle", "blogtext", "createdAt"],
-        include: [
-            {
-                model: User,
-                attributes: ["username"],
-            }
-        ]
 
-    });
+    try {
+        const blogData = await Blogpost.findAll({
+            attributes: ["id", "blogtitle", "blogtext", "createdAt"],
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                }
+            ]
+        })
+        const postData = blogData.map((project) => project.get({ plain: true }));
+        console.log(postData);
 
+        res.render('homepage', {
+            postData,
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+);
 
-    const postData = blogData.map((project) => project.get({ plain: true }))
-    console.log(postData);
-    // pass to veiws/layouts/homepage handlebar
-    res.render('homepage', {
-        postData,
-    })
-    // }
-    res.render('/login');
-})
-
-// adding from unit 23
-// router.get('/', (req, res) => {
-//     try {
-//         console.log("router to homepage")
-//     }
-//     catch (err) {
-//         res.status(550).json(err);
-//     }
-// })
-
-// get data from model Blogpost table 
-
-
-// } else {
-//     alert("homepage-render login")
-//     res.render('login', {
-//     // res.render("homepage", {
-//         postData
-//     });
-// }
-// } catch (err) {
-//     res.status(550).json(err);
-// }
-
-// });
 
 
 
 router.get('/login', (req, res) => {
-    // alert("router /login  ")
     if (req.session.logged_in) {
         res.redirect('/');
         return;
